9606c5cf134fe301624cd3b826a747327158513f
Daniel Moradi <Daniel.moradi15@gmail.com>
2024-05-08 15:08:28 +0200

    Testfallet är klar

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -7,18 +7,16 @@ const defaultTimeout = 10000;
 let driver;
 jest.setTimeout(1000 * 60 * 5); // 5 minuter
 
-// Det här körs innan vi kör testerna för att säkerställa att Chrome är igång
+// Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
 beforeAll(async () => {
     console.log(fileUnderTest);
-    driver = await new Builder().forBrowser("chrome").build();
+    driver = await new Builder().forBrowser("firefox").build();
     await driver.get(fileUnderTest);
 });
 
-// Allra sist avslutar vi chrome igen
+// Allra sist avslutar vi Firefox igen
 afterAll(async () => {
-    if (driver) {
     await driver.quit();
-    }
 }, defaultTimeout);
 
 test("The stack should be empty in the beginning", async () => {
